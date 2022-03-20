b60b4a33662a9b09f89700dcac08609bac5a8068
you@example.com <you@example.com>
2022-03-20 19:59:43 +0330

    modified sample method

--- order.py
+++ order.py
@@ -3,12 +3,14 @@
 import uuid
 from datetime import datetime
 from khayyam import JalaliDatetime
+from saloon import Table
+from finance import Bill
 
 
 class Order:
-    def __init__(self, in_out, bill, table):
+    def __init__(self, in_out, bill, table, items_dict):
         self.uuid = uuid.uuid4()
-        self.items_dict = dict()
+        self.items_dict = items_dict
         self.in_out = in_out
         self.datetime = datetime.now()
         self.bill = bill
@@ -16,7 +18,7 @@ class Order:
 
     @classmethod
     def sample(cls):
-        return cls(in_out='out', bill='bill', table='table')
+        return cls(in_out='out', bill=Bill.sample(), table=Table.sample(), items_dict={'123-123': 2})
 
     @property
     def jalali_datetime(self):
