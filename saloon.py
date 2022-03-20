52dd0fa6b41580ae17241ed64cafa7622f4dbf13
you@example.com <you@example.com>
2022-03-20 14:31:06 +0330

    added table model to saloon.py

--- saloon.py
+++ saloon.py
@@ -1,2 +1,19 @@
 # TODO : define class Table here
 # TODO : write its sample classmethod here
+
+import uuid
+
+
+class Table:
+    table_list = list()
+
+    def __init__(self, capacity, number, reserved):
+        self.uuid = uuid.uuid4()
+        self.capacity = capacity
+        self.number = number
+        self.reserved = reserved
+        self.is_available = True
+
+    @classmethod
+    def sample(cls):
+        return cls(capacity=4, number=2, reserved=False)
