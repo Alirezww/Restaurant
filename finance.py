4f5d413dc132c63b5b7b6ca7a6fa1806876e2d8d
you@example.com <you@example.com>
2022-03-20 14:10:23 +0330

    added Payment  model to finance.py

--- finance.py
+++ finance.py
@@ -2,6 +2,7 @@
 # TODO : write their sample classmethod here
 
 import uuid
+from datetime import datetime
 
 
 class Bill:
@@ -13,3 +14,16 @@ class Bill:
     @classmethod
     def sample(cls):
         return cls(total_price='1200', payment='cash')
+
+
+class Payment:
+    def __init__(self, payment_type, price):
+        self.uuid = uuid.uuid4()
+        self.datetime = datetime.now()
+        self.is_paid = False
+        self.payment_type = payment_type
+        self.price = price
+
+    @classmethod
+    def sample(cls):
+        return cls(payment_type='cash', price='12000')
