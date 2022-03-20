758965c9070000a71465154a9b830a7504c281fa
you@example.com <you@example.com>
2022-03-20 14:39:14 +0330

    added order model to order.py

--- order.py
+++ order.py
@@ -1,2 +1,16 @@
 # TODO : define class Order here
 # TODO : write its sample classmethod here
+import uuid
+
+
+class Order:
+    def __init__(self, in_out, bill, table):
+        self.uuid = uuid.uuid4()
+        self.items_dict = dict()
+        self.in_out = in_out
+        self.bill = bill
+        self.table = table
+
+    @classmethod
+    def sample(cls):
+        return cls(in_out='out', bill='bill', table='table')
