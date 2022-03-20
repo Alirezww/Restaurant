b17b02e4844a54b72bc8d911e22fa7b46110d84f
you@example.com <you@example.com>
2022-03-20 14:06:11 +0330

    added bill model to finance.py

--- finance.py
+++ finance.py
@@ -1,2 +1,15 @@
 # TODO : define class Bill and Payment here
 # TODO : write their sample classmethod here
+
+import uuid
+
+
+class Bill:
+    def __init__(self, total_price, payment):
+        self.uuid = uuid.uuid4()
+        self.total_price = total_price
+        self.payment = payment
+
+    @classmethod
+    def sample(cls):
+        return cls(total_price='1200', payment='cash')
