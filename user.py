73f38c9765798cfa4fd19220157b0ea0d6cd5d64
you@example.com <you@example.com>
2022-03-20 13:47:20 +0330

    added Supervisor model to user.py

--- user.py
+++ user.py
@@ -1,2 +1,14 @@
 # Todo : define class Supervisor here
 # TODO : write their sample classmethod
+
+class Supervisor:
+    def __init__(self, username, password, phone_number):
+        self.username = username
+        self.password = password
+        self.phone_number = phone_number
+
+    @classmethod
+    def sample(cls):
+        return cls(
+            username='alireza', password='1234', phone_number='9131271388'
+        )
