c6a7a853b08d188d26280bd7840c6aef9958fc54
you@example.com <you@example.com>
2022-03-21 12:42:37 +0330

    modified Order model

--- order.py
+++ order.py
@@ -3,23 +3,32 @@
 import uuid
 from datetime import datetime
 from khayyam import JalaliDatetime
-from saloon import Table
 from finance import Bill
 
 
 class Order:
-    def __init__(self, in_out, bill, table, items_dict):
+    def __init__(self, in_out, items_dict, number_table):
         self.uuid = uuid.uuid4()
         self.items_dict = items_dict
         self.in_out = in_out
         self.datetime = datetime.now()
-        self.bill = bill
-        self.table = table
+        self.bill = self.create_bill()
+        self.table = self.assign_table(number_table)
 
     @classmethod
     def sample(cls):
-        return cls(in_out='out', bill=Bill.sample(), table=Table.sample(), items_dict={'123-123': 2})
+        return cls(in_out='out', items_dict={'123-123': 2}, number_table=3)
+
+    def assign_table(self, number):
+        pass
+
+    def create_bill(self):
+        return Bill(self.total_price)
 
     @property
     def jalali_datetime(self):
         return JalaliDatetime(self.datetime)
+
+    @property
+    def total_price(self):
+        return 12000  # This is just test
