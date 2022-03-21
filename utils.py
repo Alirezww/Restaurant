52a19aac8d1164f1b652010f696a02260098fe33
you@example.com <you@example.com>
2022-03-21 13:55:32 +0330

    added save_to_file method

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
+import json
+
 from constanst import SUPERVISOR_CREDENTIAL
 
 
@@ -7,3 +9,8 @@ def check_credential(user_credential):
         check_password = bool(SUPERVISOR_CREDENTIAL[0]['password'] == user_credential[2])
         return check_password, check_username
     return False
+
+
+def save_to_file(file_path, data):
+    with open(file_path, 'w') as f:
+        f.writelines(json.dumps(data))
