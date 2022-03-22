cbdd6b0d0305a9b897273b8b927a45eedcefe85d
you@example.com <you@example.com>
2022-03-22 14:15:57 +0430

    added calculate() method

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import json
-
+from menu import Item
 from constanst import SUPERVISOR_CREDENTIAL
 
 
@@ -14,3 +14,12 @@ def check_credential(user_credential):
 def save_to_file(file_path, data):
     with open(file_path, 'w') as f:
         f.writelines(json.dumps(data))
+        print('it was successfully.')
+
+
+def calculate(order_dict):
+    item = Item.search_by_id(uuid=order_dict['uuid'])
+    if item is not None:
+        result = item.price * order_dict['count']
+        return result
+    return item
